456871431b81c4cea82946b3854fa559e6582f35
Miyoshi Ryusei <miyoshiryusei@gmail.com>
2023-08-22 20:40:06 +0900

    「http://localhost:8080/loginForm」へアクセスすると「Whitelabel Error Page」となり表示されない。

--- src/main/java/com/example/controller/LoginController.java
+++ src/main/java/com/example/controller/LoginController.java
@@ -1,5 +1,12 @@
 package com.example.controller;
 
-public class LoginController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class LoginController {
+    @GetMapping("/loginForm")
+    public String getLogin() {
+        return "loginForm";
+    }
 }
