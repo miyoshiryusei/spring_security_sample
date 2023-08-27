536c431a8ee64f70a5bb7fad85f163b19967eb09
Miyoshi Ryusei <miyoshiryusei@gmail.com>
2023-08-27 12:39:32 +0900

    lesson5実装完了

--- src/main/java/com/example/entity/User.java
+++ src/main/java/com/example/entity/User.java
@@ -27,6 +27,8 @@ public class User {
     @Column(name = "PASSWORD")
     private String password;
 
+    @Column(name = "ROLE")
+    private String role;
 
     public Integer getId() {
         return this.id;
@@ -58,5 +60,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
     }
 }
